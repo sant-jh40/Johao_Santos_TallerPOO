76d614ea49fbc1b9becdbd5adeda04953d6c1ad3
Alumnos <Alumnos@LabCivil2-PC31>
2026-04-01 09:23:24 -0300

    actualizacion 1.1

--- taller1/src/taller1Intento2/intento2.java
+++ taller1/src/taller1Intento2/intento2.java
@@ -34,12 +34,40 @@ public class intento2 {
 			} while (op != 3);
 			
 		} catch (NumberFormatException e) {
-			System.out.println("Error");
+			System.out.println("Error, ingrese porfavor un numero");
+			System.out.println("Le queda un intento");
 		} finally {
-			
-		}
+			try {
+				
+				do {
+					System.out.println("1) Menu de Usuarios");
+					System.out.println("2) Menu de Analisis");
+					System.out.println("3) Salir");
+					System.out.print("> ");
+					
+					
+					op = Integer.valueOf(r.nextLine());
+					switch (op) {
+					case 1:
+						System.out.println("Caso 1");
+						break;
+					case 2:
+						System.out.println("Caso 2");
+						break;
+					case 3:
+						System.out.println("adios");
+						break;
+					default:
+						System.out.println("Ingrese una opcion valida (1, 2 o 3)");
+					}
+				} while (op != 3);
+			} catch (NumberFormatException e) {
+				System.out.println("Cerrando archivo...");
+			}
+		
 		
 		
 
 	}
+}
 }
